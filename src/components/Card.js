35143cfa078c6aa462ac27911da0c863ce8c370b
susahu020 <surajsahu020@gmail.com>
2023-09-07 19:24:30 +0530

    upload react project

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Card({id, image, name, info, price, removeTour}) {
+
+    const [readmore, setReadmore] = useState(false);
+    const description = readmore ? info : `${info.substring(0,200)}....`;
+    function readmorehandler(){
+        setReadmore(!readmore);
+    }
+
+  return (
+    <div className='card'>
+        <img src={image} alt="img not found" className='image' />
+       
+       <div className="tour-info">
+             <div className="tour-details">
+            <h4 className='tour-price'>₹ {price}</h4>
+                <h4 className='tour-name'>{name}</h4>
+            </div>
+            <div className="description">
+                {description}
+                <span className='read-more' onClick={readmorehandler}>
+                    {readmore ? `Show Less` : `Show More`}
+                </span>
+            </div>
+       </div>
+       <button className='btn-red' onClick={()=>removeTour(id)}>
+            Not Interested
+       </button>
+    </div>
+  )
+}
+
+export default Card
